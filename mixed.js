b056b98d51760a68a665ac7edf915818361ec213
Gustavo Pajaro <gpajaro19@gmail.com>
2024-05-21 19:44:45 -0300

    add array

--- mixed.js
+++ mixed.js
@@ -1,1 +1,11 @@
-console.log('Hola q ace');
+const phrases = {
+    knock: ['knock, knock, '],
+    who: ['who\'s there, '],
+    joke1: ['water. wather who?. water you doing today?'],
+    joke2: ['Waddle. Waddle who?. Waddle it take to get you to open the door?'],
+    joke3: ['Radio. Radio who? Radio not, here I come!'],
+    joke4: ['Thermos. Thermos who? Thermos be a better punchline than this!'],
+    joke5: ['Ice cream. Ice cream who?. ICE CREAM SO YOU CAN HEAR ME!'],
+    joke6: ['Weirdo. Weirdo who?. Weirdo you think you\'re going?'],
+    joke7: ['Canoe. Canoe who?. Canoe come out now?'],
+};
